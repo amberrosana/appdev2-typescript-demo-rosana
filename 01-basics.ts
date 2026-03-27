11d785cc16efa54742f9d41495c0e84b851cc2cf
amber <amberprincessrosana05@gmail.com>
2026-03-27 10:22:17 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,20 +1,20 @@
-// console.log("Hello World!")
+console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Amber";
+let userAge: number = 19;
 
 // ...
 
-userAge = "31"
+userAge = 20
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
 
-function add(a: number, b = 5) {
+function add(a: number, b: number = 5): number {
     return a + b
 }
 
-add(10)
-// add('10')
-add(10, 6)
-// add(10, '6')
+console.log(add(10)) // 15
+console.log(add('10')) // 105
+console.log(add(10, 6)) // 16
+console.log(add(10, '6')) // 106
