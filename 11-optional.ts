3323b9b5e2884d09bfb92533dcbac6a77d15b6c9
amber <amberprincessrosana05@gmail.com>
2026-04-01 17:46:55 +0800

    Done playing and updating the 11-optional.ts file

--- 11-optional.ts
+++ 11-optional.ts
@@ -1,32 +1,48 @@
 // Optional Values in parameter
-function generateError(msg?: string) {
-    throw new Error(msg)
+function generateError(message?: string) {
+    throw new Error(message)
 }
 
-generateError("An error occured") // Automatic Semicolon Insertion pitfall - add ; to explicitly tell that this is a function
-// generateError()
-
+generateError("An error occured"); // Automatic Semicolon Insertion pitfall - add ; to explicitly tell that this is a function
+generateError() // OK, message is optional
 
 (() => {
     // Optional Values in object properties
     type User = {
         name: string;
         age: number;
-        role?: 'admin' | 'guess'
+        role?: 'admin' | 'moderator', // optional property
+        greet?: () => string // optional method
     }
 
     let user: User = {
-        name: 'Elmer',
-        age: 31
+        name: 'Amber',
+        age: 20
     }
 
     user.name
     user.age
+
+    let user2: User = {
+        name: 'Amber',
+        age: 20,
+        role: 'admin',
+        greet: function(): string {
+            return `Hello! My name is ${this.name}.`
+        }
+    }
+
+    user2.name
+    user2.age
+    user2.role
+    user2.greet!() 
 })
 
 // Nullish Coalescing `??` operator 
-let input = ''
-const didProvideInput = input ?? false;
+let input: string = ''
+const didProvideInput = input ?? false; // 
+
+const didProvideInput2 = input || false;
 
 // try it on browser console log to see results
 // update it `??` to `||`
