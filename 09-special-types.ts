d8cfbe360ad289ef17072b79f70cb93247b4facf
amber <amberprincessrosana05@gmail.com>
2026-04-01 17:29:41 +0800

    Done playing and updating the 09-special-types.ts file

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -2,24 +2,36 @@
 let a: null;
 
 a = null
-// a = "Hi"
+a = 0 // Error: Type '0' is not assignable to type 'null'.
+a = "Hi" // Error: Type '"Hi"' is not assignable to type 'null'.
+a = true // Error: Type 'true' is not assignable to type 'null'.
+a = undefined // Error: Type 'undefined' is not assignable to type 'null'.
 
 let b: null | string
 
 b = null
 b = "Hi"
-// b = 10
+b = 100 // Error: Type '10' is not assignable to type 'null | string'.
+b = true // Error: Type 'true' is not assignable to type 'null | string'.
+b = undefined
+b = [2, 3, 4] // Error: Type 'number[]' is not assignable to type 'null | string'.
+b = { name: "Amber" } // Error: Type '{ name: string; }' is not assignable to type 'null | string'.
 
 // Special Type - undefined
 let c: undefined;
 
 c = undefined
-// c = null
-// c = "Hi"
+c = "Hello" // Error: Type '"Hello"' is not assignable to type 'undefined'.
+c = 100 // Error: Type '10' is not assignable to type 'undefined'.
+c = true // Error: Type 'true' is not assignable to type 'undefined'.
+c = null // Error: Type 'null' is not assignable to type 'undefined'.
 
 let d: undefined | string
 
 d = undefined
 d = "Hi"
-// d = null
-// d = 10
+d = 100 // Error: Type '10' is not assignable to type 'undefined | string'.
+d = true // Error: Type 'true' is not assignable to type 'undefined | string'.
+d = null // Error: Type 'null' is not assignable to type 'undefined | string'.
+d = [2, 3, 4] // Error: Type 'number[]' is not assignable to type 'undefined | string'.
+d = { name: "Amber" } // Error: Type '{ name: string; }' is not assignable to type 'undefined | string'.
