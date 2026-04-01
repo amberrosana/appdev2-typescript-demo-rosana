da52a81a4d753b06a903bcfd52bf0254db6cc8ff
amber <amberprincessrosana05@gmail.com>
2026-04-01 17:01:55 +0800

    Done playing and updating the 07-custom-type-role.ts file

--- 07-custom-type-role.ts
+++ 07-custom-type-role.ts
@@ -1,21 +1,27 @@
 (() => {
     // Type Aliases or Custom Types
     // use `type` keyword to create custom type
-    type Role = 'admin' | 'guess' | 'teacher' | 'student'
+    type UserRole = 'superadmin' | 'admin' | 'moderator' | 'user' | 'guest';
 
     // Complex Object Type Definition
     type User = {
         name: string;
         age: number;
-        role: Role; // you can use as well the Role type in Object type
+        role: UserRole; // you can use as well the UserRole type in Object type
         permission: string[];
     }
 
-    let userRole: Role = 'admin'
+    let userRole: UserRole = 'admin'
 
-    // ...
+    userRole = 'teacher' // Error: Type '"teacher"' is not assignable to type 'UserRole'.
+    userRole = 0 // Error: Type '0' is not assignable to type 'UserRole'.
+    userRole = true // Error: Type 'true' is not assignable to type 'UserRole'.
 
-    function access(role: Role) {
-        // ...
+    function access(role: UserRole) {
+        role = 'superadmin' // OK
+        role = 'admin' // OK
+        role = 0 // Error: Type '0' is not assignable to type 'UserRole'.
+        role = true // Error: Type 'true' is not assignable to type 'UserRole'.
+        role = 'teacher' // Error: Type '"teacher"' is not assignable to type 'UserRole'.
     }
 })
